8e3e7b0da9f2fef97b8db9dceeb4047c8e231923
Aleksandr Karasev <xsandr.k@gmail.com>
2014-11-07 14:10:06 +0300

    new version 0.0.2

--- universalbus/__init__.py
+++ universalbus/__init__.py
@@ -3,7 +3,7 @@ import json
 
 
 class EventSender(object):
-    def __init__(self, login, password, host, virtual_host, exchange='kit'):
+    def __init__(self, login, password, host, virtual_host, exchange):
         self.exchange = exchange
         conn_params = pika.ConnectionParameters(
             host=host,
@@ -13,7 +13,7 @@ class EventSender(object):
         self.connection = pika.BlockingConnection(conn_params)
 
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.exchange, type='topic')
+        self.channel.exchange_declare(exchange=self.exchange, type='topic', auto_delete=False)
 
     def __del__(self):
         self.connection.close()
@@ -21,3 +21,19 @@ class EventSender(object):
     def push(self, routing_key, message):
         dumped_message = json.dumps(message)
         self.channel.basic_publish(exchange=self.exchange, routing_key=routing_key, body=dumped_message)
+
+
+class EventListener(EventSender):
+    def __init__(self, login, password, host, virtual_host, exchange, queue, bindings):
+        super(EventListener, self).__init__(login, password, host, virtual_host, exchange)
+        self.queue = queue
+        self.channel.queue_declare(queue, exclusive=False)
+
+        for binding_key in bindings:
+            self.channel.queue_bind(exchange=self.exchange_name,
+                                    queue=self.queue,
+                                    routing_key=binding_key)
+
+    def register_callback(self, callback, no_ack=True):
+        self.channel.basic_consume(callback, queue=self.queue, no_ack=no_ack)
+        self.channel.start_consuming()
